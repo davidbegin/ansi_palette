308c48367c80f92ab6dfe53040a777beec516501
David Begin <davidmichaelbe@gmail.com>
2015-09-03 20:19:08 -0700

    update and add example of use with doc string

--- examples/conversion.rb
+++ examples/conversion.rb
@@ -1,5 +1,26 @@
 require_relative "../lib/ansi_palette"
 
+include AnsiPalette
+
 puts "\n\n"
 puts %!puts Red("hello")!
 puts Red("hello")
+puts "\n\n"
+
+header = <<-HEADER
+  A Classy Comment Header
+HEADER
+
+body = <<-BODY
+  Some very interesting comment text.
+  With all these intricate details.
+  Explaining everything you could ever
+  want to know.
+BODY
+
+doc = <<-DOC
+  #{Red(header)}
+    #{Cyan(body)}
+DOC
+
+puts doc
